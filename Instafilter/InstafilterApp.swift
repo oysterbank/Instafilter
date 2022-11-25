12705879f5e4b3f056a042541f41a1898dc08670
Kris Laratta <krislaratta@gmail.com>
2022-11-25 06:53:42 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Kris Laratta on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
